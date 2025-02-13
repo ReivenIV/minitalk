bc99bd62e547eed545f5b45e1775b3657b5b33e2
ReivenIV <runyalen88@duck.com>
2025-02-13 18:20:31 +0100

    .h file created

---
+++ minitalk.h
@@ -0,0 +1,9 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+// # include <string.h>
+# include <stdlib.h>
+# include <unistd.h>		// for write function
+# include <signal.h>		// main lib of these project
+
+#endif
