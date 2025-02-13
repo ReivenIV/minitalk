6bc8eb746f82871a467f68b9572976e26863215a
ReivenIV <runyalen88@duck.com>
2025-02-13 18:25:56 +0100

    .h tools added

--- minitalk.h
+++ minitalk.h
@@ -1,9 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rita <rita>                                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/13 18:24:29 by rita              #+#    #+#             */
+/*   Updated: 2025/02/13 18:24:29 by rita             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
 # define MINITALK_H
 
-// # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>		// for write function
 # include <signal.h>		// main lib of these project
+
+//		-------------
+//		::  tools  ::
+//		-------------
+int		ft_strlen(char *s);
+void	ft_putstr(char *s);
+void	ft_putnbr(int n);
+int		ft_atoi(const char *str);
+
 
 #endif
