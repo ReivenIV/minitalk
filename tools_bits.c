7bd029fb5b11d6a54cff53a15164058577540cdc
ReivenIV <runyalen88@duck.com>
2025-02-14 14:19:42 +0100

    tools_signals.c  created

---
+++ tools_bits.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tools_signals.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rita <rita>                                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/14 13:44:24 by rita              #+#    #+#             */
+/*   Updated: 2025/02/14 13:44:24 by rita             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+//		----------------
+//		::  server.c  ::
+//		----------------
+/*
+we define the value of SIGUSR1 & SIGUSR2 and we added to 'c' 
+in the bit realm: we move one position to the left inside 'c' byte(8bits)
+to the left.  
+*/
+char	handle_bit(char c, int signal, int *bit_position)
+{
+	int bit_value;
+
+	// We define values
+	if (signal == SIGUSR1)
+		bit_value = 1;
+	if (signal == SIGUSR2)
+		bit_value = 0;
+
+	c = c << 1 | bit_value;		// We move 1 bit to the left then we update the current value with "bit_value".
+	(*bit_position)++;			// we update the bit count.
+	return (c);
+}
+
+
+// TODO
+// static void signals_parser(int sig, siginfo_t *info, void *context)
+// {
+	
+// }
