753c44df23b1b3ac60c6a5549ba2cdef4bcf6b63
ReivenIV <runyalen88@duck.com>
2025-02-14 14:20:12 +0100

    putnbr moved to tools_nbs

--- tools_strs.c
+++ tools_strs.c
@@ -28,28 +28,6 @@ void	ft_putstr(char *s)
 	write(1, s, ft_strlen(s));
 }
 
-void	ft_putnbr(int n)
-{
-	char	c;
-
-	if (n == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-		return ;
-	}
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		n = n * -1;
-	}
-	if (n > 9)
-	{
-		ft_putnbr(n / 10);
-	}
-	c = (n % 10) + '0';
-	write(1, &c, 1);
-}
-
 int	ft_atoi(const char *str)
 {
 	int	i;
