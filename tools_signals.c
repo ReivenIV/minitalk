56ad1ff07732f82c7e70499cce40fdce9b5ea97b
ReivenIV <runyalen88@duck.com>
2025-02-14 17:14:53 +0100

    "set_signal_action" SA_RESTART removed. Comments added

--- tools_signals.c
+++ tools_signals.c
@@ -14,30 +14,31 @@
 
 void set_signal_action(void(*handler)(int, siginfo_t*, void*), int sigusr)
 {
-	struct	sigaction	sig;
+    struct	sigaction	sig; 														// Declare a sigaction structure
 
-	sig.sa_sigaction = handler;
-	sigemptyset(&sig.sa_mask);
-	sig.sa_flags = SA_SIGINFO | SA_RESTART;
-	if (sigusr == 1)
-		sigaction(SIGUSR1, &sig, NULL);
-	else if (sigusr == 2)
-		sigaction(SIGUSR2, &sig, NULL);
-	else
-	{
-		sigaction(SIGUSR1, &sig, NULL);
-		sigaction(SIGUSR2, &sig, NULL);
-	}
+    sig.sa_sigaction = handler;														// Set the handler function for the signal
+    sigemptyset(&sig.sa_mask); 														// Initialize the signal mask to empty
+    sig.sa_flags = SA_SIGINFO;				 										// Set flags to get detailed signal info and restart interrupted system calls
+
+    if (sigusr == 1)
+        sigaction(SIGUSR1, &sig, NULL); 											// Set the action for SIGUSR1
+    else if (sigusr == 2)
+        sigaction(SIGUSR2, &sig, NULL); 											// Set the action for SIGUSR2
+    else
+    {
+        sigaction(SIGUSR1, &sig, NULL); 											// Set the action for SIGUSR1
+        sigaction(SIGUSR2, &sig, NULL); 											// Set the action for SIGUSR2
+    }
 }
 
 void	signal_parser(int signal, siginfo_t *info, void *context)
 {
 	static char	c = 0;
-	static int	bit = 0;
+	static int	bit_count = 0;
 
 	(void)context;
-	c = handle_incoming_bit(c, signal, &bit);
-	if (bit == 8)
+	c = handle_incoming_bit(c, signal, &bit_count);
+	if (bit_count == 8)
 	{
 		if (c == '\0')
 		{
@@ -52,7 +53,7 @@ void	signal_parser(int signal, siginfo_t *info, void *context)
 				return ;
 		}
 		c = 0;
-		bit = 0;
+		bit_count = 0;
 	}
 	kill(info->si_pid, SIGUSR1);
 }
