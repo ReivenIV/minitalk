63f78d8e23e481af7573bec76e760a830e1d4735
ReivenIV <runyalen88@duck.com>
2025-02-17 12:07:00 +0100

    client : work in progress

--- client.c
+++ client.c
@@ -12,27 +12,35 @@
 
 #include "minitalk.h"
 
+volatile	sig_atomic_t	g_acknowledgment_status = 0;
+
 // TODO
 /*
 	- check a way to declare g_received_ack in .h
 	- what is volatile ? 
 	- ack_handler : why ? 
 	- wait_sig : 	why ? 
-
-
 */
+
+
 
 int	main(int ac, char **av)
 {
 	int	pid;
 
-	if (ac !=)
+	if (ac != 3)
+	{
+		write(2, "Something wrong my dear\n", 24);
 		return (0);
+	}
 	if (ac == 3)
 	{
 		pid = ft_atoi(av[1]);
 		if (kill(pid, 0) == -1)
+		{
+			write(2, "Wrong PID my dear\n", 18);
 			return (0);
+		}
 		if (av[2] == NULL)
 			return (0);
 		set_signal_action(send_msg, 3);	// TODO send_msg 
