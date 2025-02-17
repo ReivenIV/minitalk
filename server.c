0654e85d0d7c11badf2e3c292cf68889f88b5740
ReivenIV <runyalen88@duck.com>
2025-02-17 13:42:35 +0100

    server updated with g variable and functions related to it

--- server.c
+++ server.c
@@ -14,8 +14,38 @@
 
 char *global_str = NULL;
 
+// Server : 
+void	signal_parser(int signal, siginfo_t *info, void *context)
+{
+	static char	c = 0;
+	static int	bit_count = 0;
+
+	(void)context;
+	c = handle_incoming_bit(c, signal, &bit_count);
+	if (bit_count == 8)
+	{
+		if (c == '\0')
+		{
+			ft_putstr(global_str);
+			free(global_str);
+			global_str = NULL;
+		}
+		else
+		{
+			global_str = update_global_str(global_str, c);
+			if (global_str == NULL)
+				return ;
+		}
+		c = 0;
+		bit_count = 0;
+	}
+	kill(info->si_pid, SIGUSR1);
+}
+
+
 int	main(int ac, char **av)
 {
+	(void)	av;
 	if (ac == 1)
 	{
 		write(1, "Server PID my dear :: ", 22);
