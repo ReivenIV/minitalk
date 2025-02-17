7a11cbf1caf76d9916aaacbe0deb2fcad1cd143d
ReivenIV <runyalen88@duck.com>
2025-02-17 12:07:12 +0100

    .h updates ,comment and funcs

--- minitalk.h
+++ minitalk.h
@@ -28,6 +28,8 @@ extern char	*global_str;
 int		ft_strlen(char *s);
 void	ft_putstr(char *s);
 int		ft_atoi(char *str);
+
+// server : 
 char	*update_global_str(char *str, char c);
 
 //		-----------------
@@ -38,12 +40,26 @@ void	ft_putnbr(int n);
 //		------------------
 //		::  tools_bits  ::
 //		------------------
+
+// server: 
 char	handle_incoming_bit(char c, int signal, int *bit_position);
+
+// client :
+void	handle_send_signal(int pid, char *str);
 
 //		---------------------
 //		::  tools_signals  ::
 //		---------------------
+
+// global :
 void 	set_signal_action(void(*handler)(int, siginfo_t*, void*), int sigusr);
+
+// server : 
 void	signal_parser(int signal, siginfo_t *info, void *context);
+
+// client : 
+void	stablish_link_with_server(int signum, siginfo_t *info, void *content);
+void	wait_validation_from_server(void);
+
 
 #endif
